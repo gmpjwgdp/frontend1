c191559fc7ada6a7215587eabd5f533910c2c263
gmpjwgdp <138013198+gmpjwgdp@users.noreply.github.com>
2023-06-28 21:04:36 +0900

    Add files via upload

---
+++ sample2_4_portfolio/sample2_4_portfolio.js
@@ -0,0 +1,84 @@
+jQuery(document).ready(function($){
+  $(".list1").hover(
+    function(){
+      jQuery(document).ready(function($){
+          $(".list1").css("background","rgba(255,192,203,0.4)");
+        });
+      },
+    function(){
+      jQuery(document).ready(function($){
+        $(".list1").css("background","white");
+      });
+    }
+  );
+});
+jQuery(document).ready(function($){
+  $(".list2").hover(
+    function(){
+      jQuery(document).ready(function($){
+          $(".list2").css("background","rgba(137,189,222,0.4)");
+        });
+      },
+    function(){
+      jQuery(document).ready(function($){
+        $(".list2").css("background","white");
+      });
+    }
+  );
+});
+jQuery(document).ready(function($){
+  $(".list3").hover(
+    function(){
+      jQuery(document).ready(function($){
+          $(".list3").css("background","rgba(255,165,0,0.4)");
+        });
+      },
+    function(){
+      jQuery(document).ready(function($){
+        $(".list3").css("background","white");
+      });
+    }
+  );
+});
+jQuery(document).ready(function($){
+  $(".list4").hover(
+    function(){
+      jQuery(document).ready(function($){
+          $(".list4").css("background","rgba(0,128,0,0.4)");
+        });
+      },
+    function(){
+      jQuery(document).ready(function($){
+        $(".list4").css("background","white");
+      });
+    }
+  );
+});
+jQuery(document).ready(function($){
+  $(".list5").hover(
+    function(){
+      jQuery(document).ready(function($){
+          $(".list5").css("background","rgba(128,128,128,0.4)");
+        });
+      },
+    function(){
+      jQuery(document).ready(function($){
+        $(".list5").css("background","white");
+      });
+    }
+  );
+});
+// jQuery(document).ready(function($){
+//   $("button").hover(
+//     function(){
+//       jQuery(document).ready(function($){
+//         $(this).css("opacity","0.4");
+//         console.log($(this));
+//       });
+//     },
+//   function(){
+//     jQuery(document).ready(function($){
+//       $("button").css("background","white");
+//     });
+//   });
+// });
